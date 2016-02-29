cfdedbf97d7e76f30680bf7a9b5e3e5a00003fdf
irmowan <irmowan@gmail.com>
2016-02-29 09:24:31 +0800

    Get target tab in window

--- background.js
+++ background.js
@@ -1,32 +1,30 @@
 
-function getMailUrl() {
-    return "https://mail.fudan.edu.cn/";
+// TODO: AutoSSL
+function autoSSL() {
+
 }
 
-function isMailUrl() {
-    alert(url.indexOf(getMailUrl()));
+function getMailUrl() {
+    return "http://mail.fudan.edu.cn/";
+}
+
+function isMailUrl(url) {
     return url.indexOf(getMailUrl()) == 0;
 }
 
 function goToInbox() {
     console.log('Going to fudan mail inbox...');
-    // TODO: Fix Bug
     chrome.tabs.getAllInWindow(undefined, function(tabs) {
-        // for (var i = 0, tab; tab = tabs[i]; i++) {
-        //     alert(tab.url);
-        //     if (tab.url && isMailUrl(tab.url)) {
-        //         alert("found");
-        //         console.log('Found fudan mail tab: ' + tab.url + '. ' +
-        //             'Focusing and refreshing count...');
-        //         chrome.tabs.update(tab.id, {selected: true});
-        //         startRequest({
-        //             scheduleRequest: false,
-        //             showLoadingAnimation: false
-        //         });
-        //         return;
-        //     }
-        // }
-        // console.log('Could not find Fudan mail tab. Creating one...');
+        for (var i = 0, tab; tab = tabs[i]; i++) {
+            if (tab.url && isMailUrl(tab.url)) {
+                console.log('Found fudan mail tab: ' + tab.url + '. ' +
+                    'Focusing and refreshing count...');
+                chrome.tabs.update(tab.id, {selected: true});
+                startRequest({scheduleRequest: false, showLoadingAnimation: false});
+                return;
+            }
+        }
+        console.log('Could not find Fudan mail tab. Creating one...');
         chrome.tabs.create({url: getMailUrl()});
     });
 }
